875f8dc8b1778d5192c246a95d381d53628229b2
xiaoyulyu999 <info@bloomora.net>
2025-05-05 21:03:19 +0100

    inventory

---
+++ inventory-service/src/test/java/com/java_microserver/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_microserver.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
