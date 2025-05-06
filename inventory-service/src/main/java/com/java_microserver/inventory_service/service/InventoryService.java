d073b9d550792900a594bb97eddf1a742da598d3
xiaoyulyu999 <info@bloomora.net>
2025-05-06 23:06:14 +0100

    inventory

--- inventory-service/src/main/java/com/java_microserver/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/java_microserver/inventory_service/service/InventoryService.java
@@ -14,6 +14,7 @@ public class InventoryService {
 
     @Transactional(readOnly = true)
     public boolean isInStock(String skuCode) {
+
         return inventoryRepository.findBySkuCode(skuCode).isPresent();
     }
 
