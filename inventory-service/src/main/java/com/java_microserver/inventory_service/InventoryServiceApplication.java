2fdf95c0f679322fe3436cb66e54e2cc7deb4461
xiaoyulyu999 <info@bloomora.net>
2025-06-14 15:27:38 +0100

    inventory

--- inventory-service/src/main/java/com/java_microserver/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/java_microserver/inventory_service/InventoryServiceApplication.java
@@ -1,36 +1,12 @@
 package com.java_microserver.inventory_service;
 
-import com.java_microserver.inventory_service.Repository.InventoryRepository;
-import com.java_microserver.inventory_service.model.Inventory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
-
-
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-
-		return args -> {
-			Inventory inventory = new Inventory();
-			Inventory inventory2 = new Inventory();
-
-			inventory.setSkuCode("iphone_19");
-			inventory.setQuantity(100);
-
-			inventory2.setSkuCode("iphone_20");
-			inventory2.setQuantity(200);
-
-			inventoryRepository.save(inventory);
-			inventoryRepository.save(inventory2);
-		};
-
 	}
 }
